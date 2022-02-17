1f5a8003196e1cbe87ffc59669816cbe73d9675c
flipChandler <felipe.santos11@hotmail.com>
2022-02-16 21:15:21 -0300

    rest template post for object, exchange post, put and delete

--- src/main/java/academy/devdojo/animeapi/client/SpringClient.java
+++ src/main/java/academy/devdojo/animeapi/client/SpringClient.java
@@ -3,6 +3,7 @@ package academy.devdojo.animeapi.client;
 import academy.devdojo.animeapi.domain.Anime;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -31,5 +32,32 @@ public class SpringClient {
                 new ParameterizedTypeReference<List<Anime>>() {
                 });
         log.info(exchange.getBody());
+
+
+        Anime anime2 = Anime.builder().name("Hunter x Hunter").build();
+        Anime savedAnime2 = new RestTemplate().postForObject("http://localhost:8081/api/v1/animes", anime2, Anime.class);
+        log.info("saved anime {}", savedAnime2);
+
+        Anime anime3 = Anime.builder().name("One Punchman").build();
+        ResponseEntity<Anime> savedAnime3 = new RestTemplate().exchange("http://localhost:8081/api/v1/animes",
+                HttpMethod.POST,
+                new HttpEntity<>(anime3),
+                Anime.class);
+        log.info("saved anime {}", savedAnime3);
+
+        Anime anime4 = savedAnime3.getBody();
+        anime4.setName("One Punchman Atualizado");
+        ResponseEntity<Void> updatedAnime4 = new RestTemplate().exchange("http://localhost:8081/api/v1/animes",
+                HttpMethod.PUT,
+                new HttpEntity<>(anime4),
+                Void.class);
+        log.info("updated anime {}", updatedAnime4);
+
+        ResponseEntity<Void> deleteddAnime4 = new RestTemplate().exchange("http://localhost:8081/api/v1/animes/{id}",
+                HttpMethod.DELETE,
+                null,
+                Void.class,
+                anime4.getId());
+        log.info("deleted anime {}", deleteddAnime4);
     }
 }
